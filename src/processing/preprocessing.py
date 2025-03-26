0d495a94991378c823ba0f5d793177f182aabd81
Simon Flagerup <simon.flagerup@afry.com>
2025-03-26 09:58:23 +0100

    changed preprocessing to be the entire pipelina and remove outliers to only remove the outliers

--- src/processing/preprocessing.py
+++ src/processing/preprocessing.py
@@ -1,54 +1,34 @@
 import numpy as np
 
+from src.processing import remove_outliers
 from src.processing import nan_regions #nan_regions.py
+from src.processing import shift_region
 from src.processing import interpolate_nan #interpolate_nan.py
 
-def clean_zeros_outliers_interpolate(df):
+def preprocessing_pipeline(df):
 
-    #Find values of strain that are exactly zero
-    df_zero = df[df["Strain"] == 0]
-    print(f'{df_zero.shape[0]} zeros to replace with NaN')
+    # Remove zeros and outliers
+    df = remove_outliers.clean_zeros_outliers(df)
 
-    # Remove rows with strain values of zero by replacing with NaN
-    df = df.copy()
-    df.loc[df["Strain"] == 0, "Strain"] = np.nan
+    # Find consecutive NaN regions
+    threshold = 5 # Choose the threshold(s) for consecutive NaNs
+    consecutive_nan_regions, nan_regions_sorted = nan_regions.find_nan_regions(df, threshold)
 
-    # Compute IQR
-    Q1 = df['Strain'].quantile(0.25)  # 25th percentile
-    Q3 = df['Strain'].quantile(0.75)  # 75th percentile
-    IQR = Q3 - Q1
+    # Shift regions 
+    n_points = 15
+    std_multiplier = 4.6
+    min_region_size = 5
 
-    # Define bounds for extreme outliers
-    lower_bound = Q1 - 1.5 * IQR
-    lower_bound_extreme = Q1 - 3 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-    upper_bound_extreme = Q3 + 3 * IQR
+    df = shift_region.shift_scale_diff(df, std_multiplier, n_points, n_points, min_region_size)
 
-    mild_outliers = df[(df['Strain'] < lower_bound) | (df['Strain'] > upper_bound)]
-    mild_outlier_indices = mild_outliers.index  # Save the indices of mild outliers
-    num_mild_outliers = mild_outliers.shape[0]
+    # Interpolate NaN regions shorter than the threshold
+    interpolate_threshold = 5
 
-    # Count extreme outliers (outside 3 * IQR)
-    extreme_outliers = df[(df['Strain'] < lower_bound_extreme) | (df['Strain'] > upper_bound_extreme)]
-    extreme_outlier_indices = extreme_outliers.index  # Save the indices of extreme outliers
-    num_extreme_outliers = extreme_outliers.shape[0]
+    df_interpolated = interpolate_nan.interpolate(df, nan_regions_sorted, interpolate_threshold)
 
-    # Print results
-    print(f'Number of mild outliers (1.5 × IQR): {num_mild_outliers}')
-    print(f'Number of extreme outliers (3 × IQR): {num_extreme_outliers}')
+    df = df_interpolated
 
-    # Replace extreme outliers with NaN
-    df.loc[mild_outlier_indices, 'Strain'] = np.nan  # Using np.nan to replace the outlier values
-    print(f'Number of outliers replaced with NaN: {len(mild_outlier_indices)}')
-
+    # Add function that scales the data here
+    # df = scale_data(df)
 
     return df
-
-'''
-    consecutive_nan_regions, nan_regions_sorted = nan_regions.find_nan_regions(df, threshold=1)
-
-    df_filled = interpolate_nan.interpolate(df, nan_regions_sorted, interpolate_threshold)
-    df = df_filled
-
-    return df'
-'''
