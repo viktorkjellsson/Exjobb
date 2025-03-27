ae77d4eda20f20910167b40217594a0f044c7288
Viktor Kjellsson <viktor.kjellsson@afry.com>
2025-03-27 08:50:52 +0100

    adjusted eda

--- configs/path_config.py
+++ configs/path_config.py
@@ -24,3 +24,23 @@ TXT_OUTPUT_DIR = BASE_DIR / 'output' / 'txt'
 
 # Configuration directories
 CONFIG_DIR = BASE_DIR / 'configs'
+
+# =============================
+# Loop configurations
+# =============================
+
+GROUP1 = ['N-B_Far_Comp.txt_N13, B, 0.03.csv',
+        'N-B_Mid1_Comp.txt_N5, B, 0.02.csv',
+        'N-B_Mid2_Comp.txt_N10, B, 0.11.csv',
+        'N-B-Close_Comp.txt_IX, B, 0.04.csv',
+        'N-C_Close2_Comp.txt_IX, C, 0.09.csv',
+        'N-C_Far_Comp.txt_N13, C, 0.03.csv',
+        'N-C_Mid_Comp.txt_N10, C, 0.04.csv',
+        'N-D_Far_Comp.txt_N13, D, 0.02.csv',
+        'N-E_Close_Comp.txt_IX, E, 0.04.csv',
+        'N-E_Far_Comp.txt_N13, E, 0.03.csv',
+        'N-E_Mid1_Comp.txt_N5, E, 0.03.csv',
+        'N-F_Close_Comp.txt_IX, F, 0.04.csv',
+        'N-F_Mid_Comp.txt_N13, F, 0.02.csv',
+        'N-Klaff_Comp.txt_V, B, 1.65.csv'
+        ]
