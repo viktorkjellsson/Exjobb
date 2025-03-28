111274b4414eb9041ea524b9e67ad9fbad1cc800
Viktor Kjellsson <viktor.kjellsson@afry.com>
2025-03-28 13:38:52 +0100

    data loader

--- configs/path_config.py
+++ configs/path_config.py
@@ -29,6 +29,7 @@ CONFIG_DIR = BASE_DIR / 'configs'
 # Loop configurations
 # =============================
 
+# GROUP1 - one of the subsets corresponting to loops with similarity in available data
 GROUP1 = ['N-B_Far_Comp.txt_N13, B, 0.03.csv',
         'N-B_Mid1_Comp.txt_N5, B, 0.02.csv',
         'N-B_Mid2_Comp.txt_N10, B, 0.11.csv',
@@ -43,4 +44,16 @@ GROUP1 = ['N-B_Far_Comp.txt_N13, B, 0.03.csv',
         'N-F_Close_Comp.txt_IX, F, 0.04.csv',
         'N-F_Mid_Comp.txt_N13, F, 0.02.csv',
         'N-Klaff_Comp.txt_V, B, 1.65.csv'
+        ]
+
+# GROUP1A - subset of GROUP1
+GROUP1A = ['N-B_Far_Comp.txt_N13, B, 0.03.csv',
+        'N-B_Mid1_Comp.txt_N5, B, 0.02.csv',
+        'N-B_Mid2_Comp.txt_N10, B, 0.11.csv',
+        'N-B-Close_Comp.txt_IX, B, 0.04.csv',
+        'N-C_Far_Comp.txt_N13, C, 0.03.csv',
+        'N-C_Mid_Comp.txt_N10, C, 0.04.csv',
+        'N-E_Far_Comp.txt_N13, E, 0.03.csv',
+        'N-F_Close_Comp.txt_IX, F, 0.04.csv',
+        'N-F_Mid_Comp.txt_N13, F, 0.02.csv',
         ]
